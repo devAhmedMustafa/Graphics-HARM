a4a6e8dd1c29235a75d772d927572a9b65e7a2be
HabibaIbrahim6 <habibaibrahimm998@gmail.com>
2026-05-13 13:11:26 -0700

    Update CircleAlgorithms.cpp

--- CircleAlgorithms.cpp
+++ CircleAlgorithms.cpp
@@ -1,6 +1,7 @@
 #include "CircleAlgorithms.h"
 #include <cmath>
 
+// Draw 8 symmetric points of a circle
 void Draw8Points(HDC hdc,
                  int xc,
                  int yc,
@@ -8,15 +9,106 @@ void Draw8Points(HDC hdc,
                  int y,
                  COLORREF color)
 {
+    // First quadrant reflections
     SetPixel(hdc, xc + x, yc + y, color);
     SetPixel(hdc, xc - x, yc + y, color);
     SetPixel(hdc, xc + x, yc - y, color);
     SetPixel(hdc, xc - x, yc - y, color);
 
+    // Swap x and y for remaining octants
     SetPixel(hdc, xc + y, yc + x, color);
     SetPixel(hdc, xc - y, yc + x, color);
     SetPixel(hdc, xc + y, yc - x, color);
     SetPixel(hdc, xc - y, yc - x, color);
+}
+
+
+
+// Direct Circle Algorithm
+// Uses circle equation: x^2 + y^2 = r^2
+void DirectCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
+{
+    int x = 0;
+    int y = r;
+    int R2 = r * r;
+
+    // First point (top of circle)
+    Draw8Points(hdc, xc, yc, x, y, color);
+
+    // Generate circle points from equation
+    while (x < y)
+    {
+        x++;
+        y = (int)round(sqrt((double)(R2 - x * x)));
+
+        Draw8Points(hdc, xc, yc, x, y, color);
+    }
+}
+
+// Midpoint Circle Algorithm
+void MidpointCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
+{
+    int x = 0;
+    int y = r;
+    int d = 1 - r;
+
+    // Initial point
+    Draw8Points(hdc, xc, yc, x, y, color);
+
+    // Traverse first octant
+    while (x < y)
+    {
+        if (d < 0)
+        {
+            // Move East
+            d += 2 * x + 2;
+        }
+        else
+        {
+            // Move South-East
+            d += 2 * (x - y) + 5;
+            y--;
+        }
+
+        x++;
+
+        Draw8Points(hdc, xc, yc, x, y, color);
+    }
+}
+
+// Modified Midpoint Circle Algorithm
+void ModifiedMidpointCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
+{
+    int x = 0;
+    int y = r;
+
+    int d = 1 - r;
+    int ch1 = 3;
+    int ch2 = 5 - (2 * r);
+
+    Draw8Points(hdc, xc, yc, x, y, color);
+
+    while (x < y)
+    {
+        if (d < 0)
+        {
+            // Choose E
+            d += ch1;
+            ch2 += 2;
+        }
+        else
+        {
+            // Choose SE
+            d += ch2;
+            ch2 += 4;
+            y--;
+        }
+
+        ch1 += 2;
+        x++;
+
+        Draw8Points(hdc, xc, yc, x, y, color);
+    }
 }
 
 void PolarCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
@@ -31,6 +123,7 @@ void PolarCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
         SetPixel(hdc, xc + (int)x, yc + (int)y, color);
     }
 }
+
 void IterativePolarCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
 {
     float x = r;
@@ -53,37 +146,4 @@ void IterativePolarCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
         x = x_new;
         y = y_new;
     }
-}
-
-void DirectCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
-{
-    int x = 0;
-    int y = r;
-    int R2 = r * r;
-    Draw8Points(hdc, xc, yc, x, y, color);
-    while (x < y)
-    {
-        
-        x++;
-        y = (int)round(sqrt((double)(R2 - x * x)));
-        Draw8Points(hdc, xc, yc, x, y, color);
-    }
-}
-
-
-void ModifiedMidpointCircle(HDC hdc, int xc, int yc, int r, COLORREF color)
-{
-    int x = 0 ;
-    int y = r ;
-    int d = 1-r ;
-    Draw8Points(hdc, xc, yc, x, y, color);
-    while(x<y){
-     if(d<0)  
-        d+=2*x+2; 
-     else  {
-        d+=2*(x-y)+5; 
-         y--;  } 
-    x++; 
-    }
-  Draw8Points(hdc,xc,yc,x,y,color); 
 }
